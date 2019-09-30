475b69e9508a6e65beb13e3f90066e1344fac8f9
Joshua Norris <joshua.norris52@gmail.com>
2019-09-30 10:12:23 -0500

    Initial Commit

---
+++ Train Iconic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Train Iconic
+//
+//  Created by macuser on 9/30/19.
+//  Copyright © 2019 JoshuaNorris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Train IconicTests/Train_IconicTests.swift
@@ -0,0 +1,34 @@
+//
+//  Train_IconicTests.swift
+//  Train IconicTests
+//
+//  Created by macuser on 9/30/19.
+//  Copyright © 2019 JoshuaNorris. All rights reserved.
+//
+
+import XCTest
+@testable import Train_Iconic
+
+class Train_IconicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
